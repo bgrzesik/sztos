b88ad6b3d8ad167268b4e9dc16bb3ebdfd564a15
Bartłomiej Grzesik <bartlomiej.grzesik0@gmail.com>
2022-05-08 16:16:39 +0200

    Add CurrentEL and SPsel getter

--- src/arch/aarch64/regs.rs
+++ src/arch/aarch64/regs.rs
@@ -5,12 +5,12 @@ use core::{
 };
 
 macro_rules! system_register {
-    ($name: ident) => {
+    ($name: ident, $reg: ident) => {
         #[inline(always)]
         pub unsafe fn $name() -> u64 {
             let mut reg: u64;
             asm!(
-                concat!("mrs {reg}, ", stringify!($name)),
+                concat!("mrs {reg}, ", stringify!($reg)),
                 reg = out(reg) reg
             );
             reg
@@ -21,16 +21,39 @@ macro_rules! system_register {
 pub struct SystemRegisters;
 
 impl SystemRegisters {
-    system_register!(mpidr_el1);
+    system_register!(mpidr_el1, mpidr_el1);
+    system_register!(current_el, CurrentEL);
+    system_register!(spsel, SPsel);
 }
 
 
 pub struct System;
 
+
+#[repr(u8)]
+pub enum ExceptionLevel {
+    User          = 0x00, // EL0
+    Kernel        = 0x01, // EL1
+    Hypervisor    = 0x02, // EL2
+    SecureMonitor = 0x03, // EL3
+    Unknown       = 0xff,
+}
+
 impl System {
     #[inline(always)]
     pub unsafe fn core_id() -> u16 {
         (SystemRegisters::mpidr_el1() & 0x03) as u16
+    }
+
+    #[inline(always)]
+    pub unsafe fn exception_level() -> ExceptionLevel {
+        match SystemRegisters::current_el() {
+            0b0000 => ExceptionLevel::User,
+            0b0001 => ExceptionLevel::Kernel,
+            0b0010 => ExceptionLevel::Hypervisor,
+            0b0011 => ExceptionLevel::SecureMonitor,
+                 _ => ExceptionLevel::Unknown,
+        }
     }
 }
 

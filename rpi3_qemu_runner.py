31ba53b1219415597de6a9a2f40439f7e37e67bb
Qbok <Qbok@Qbok.de>
2022-06-08 21:17:25 +0200

    Added functionality to determine suported board on qemu system

--- rpi3_qemu_runner.py
+++ rpi3_qemu_runner.py
@@ -3,9 +3,13 @@
 import sys
 import os
 
+QEMU = 'qemu-system-aarch64'
 
 def main():
-    print(sys.argv)
+    machines = os.popen(f"{QEMU} -machine help | sed '1d' | awk -F' ' '{{ print $1; }}'").read()
+    
+    machine = 'raspi3' if 'raspi3' in machines else 'raspi3b'
+
     kernel = sys.argv[1]
     kernel_bin = f'{kernel}.bin'
 
@@ -14,7 +18,7 @@ def main():
     os.system(objcopy)
 
     args = ' '.join(sys.argv[2:])
-    qemu = 'qemu-system-aarch64 -M raspi3 -smp 4 -m 1G -cpu cortex-a72 -serial stdio -monitor telnet:localhost:4444,server,nowait -display none -gdb tcp::3333 -S -kernel'
+    qemu = f'{QEMU} -M {machine} -smp 4 -m 1G -cpu cortex-a72 -serial stdio -monitor telnet:localhost:4444,server,nowait -display none -gdb tcp::3333 -S -kernel'
     qemu = f'{qemu} {kernel_bin} {args}'
     print(qemu)
     os.system(qemu)
